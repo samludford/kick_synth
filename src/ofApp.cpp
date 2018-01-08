580787399c1ea2eb399688e54d97a769a8d5b0a6
Sam Ludford <sam@Sams-MacBook-Pro.local>
2018-01-08 19:33:55 +0000

    comments and readme

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -39,16 +39,17 @@ void ofApp::setup(){
     pitch.setSustain(1.0);
     pitch.setRelease(10000.0);
     
+    // setup the FFT and octave analyser
     fft.setup(1024, 512, 256);
     oct.setup(44100, 1024, 10);
     
+    // gui
     params.add(attack.set("attack", 20.0, 1.0, 100.0));
     params.add(release.set("release", 60.0, 1.0, 900.0));
     params.add(pitch_sweep.set("sweep", 20.0, 1.0, 200.0));
     params.add(pitch_max.set("max pitch", 7000.0, 400.0, 10000.0));
     params.add(pitch_min.set("min pitch", 100.0, 20.0, 200.0));
     
-
     attack.addListener(this, &ofApp::attackChanged);
     release.addListener(this, &ofApp::releaseChanged);
     pitch_sweep.addListener(this, &ofApp::pitchSweepChanged);
@@ -72,11 +73,10 @@ void ofApp::draw(){
     float oct_spacing = (ofGetWidth() - padding*2.0) / (float) oct.nAverages;
     float amp_spacing = (ofGetWidth() - padding*2.0)  / (float) display_buffer.size();
     
-    // draw fft
     ofPushMatrix();
     ofTranslate(ofGetWidth()/2.0, ofGetHeight()/2.0);
     for(int i=0 ; i < oct.nAverages ; i++) {
-
+        
             ofNoFill();
             ofSetColor(ofMap(oct.averages[i], 0.0, 150.0, 0.0, 255.0));
             float w = ofMap(oct.averages[i], 0.0, 150.0, 0.0, ofGetWidth()-padding*2.0);
@@ -101,23 +101,30 @@ void ofApp::draw(){
 void ofApp::audioOut(float * output, int bufferSize, int nChannels) {
     
     for (int i = 0; i < bufferSize; i++){
-    
+        
+        // get the current amplitude based on the amp envelope
         amp_current = env.adsr(1.0, env.trigger);
         
+        // if envelope is triggered, untrigger it off immediately
         if(env.trigger == 1) {
             env.trigger = 0.0;
         }
         
-        
+        // get the value of the pitch envelope, based on trigger
         ramp_val = pitch.adsr(1.0, pitch.trigger);
         if(amp_current <= 0.001 ) {
+            // untrigger the pitch env only once amplitude is low enough
             pitch.trigger = 0.0;
         }
         
+        // get current frequency based on the pitch envelope and the max/min pitch
         float freq = ofMap(ramp_val, 0.0, 0.9, pitch_max, pitch_min);
-
+        
+        // get the signal of a sinewave at the frequency determined by the pitch envelope,
+        // and shape its volume with the amp envelope
         wave = myOsc.sinewave(freq) * amp_current;
         
+        // calculate fft and octave analyser
         if (fft.process(wave)) {
             fft.magsToDB();
             oct.calculate(fft.magnitudes);
@@ -126,6 +133,7 @@ void ofApp::audioOut(float * output, int bufferSize, int nChannels) {
         output[i*nChannels    ] = wave;
         output[i*nChannels + 1] = wave;
         
+        // store audio data in buffer for drawing
         display_buffer[i] = wave;
         
     }
@@ -146,6 +154,7 @@ void ofApp::audioIn(float * input, int bufferSize, int nChannels){
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
+    // when spacebar is pressed trigger amp and pitch envelopes
     if(key == ' ') {
         if(!held) {
             env.trigger = 1;
